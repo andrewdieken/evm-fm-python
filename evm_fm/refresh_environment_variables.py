49073fcd3c24898843dd30c428d72101dce1326f
andrewdieken <andrewrd@live.com>
2020-07-12 20:03:56 -0700

    Update ability to run post commands

--- evm_fm/refresh_environment_variables.py
+++ evm_fm/refresh_environment_variables.py
@@ -1,8 +1,12 @@
-from ssm_parameter_store import EC2ParameterStore
-from utils import update_environment_variable_file, parse_args
-from botocore.exceptions import NoCredentialsError
+import shlex
+import subprocess
 import sys
+
 import toml
+from botocore.exceptions import NoCredentialsError
+from ssm_parameter_store import EC2ParameterStore
+
+from utils import parse_args, update_environment_variable_file
 
 sys.tracebacklimit=0
 
@@ -21,9 +25,26 @@ def main():
     # Update env var file
     file_updated = update_environment_variable_file(s3_environment_variable_mappings=param_store_parameters, file_path=parsed_config_file['env_file_path'])
 
-    # Run post command if file was updated
+    # Run post commands if file was updated
     if file_updated:
-        pass
+        post_commands = parsed_config_file.get('post_commands', None)
+        for command_args in post_commands:
+            command = command_args.get('command', None)
+            use_shell = command_args.get('shell', False)
+
+            # The subprocess library handles the command argument differently whether the shell argument
+            # is passed as True or not. TL;DR if the shell argument is True then the command argument
+            # should be passed as a single String. See https://stackoverflow.com/a/15109975 for additional
+            # reading.
+            if not use_shell:
+                command = shlex.split(command)
+
+            try:
+                completed_command = subprocess.run(command, shell=use_shell, check=True)
+                print("Command '{}' completed successfully with exit code {}".format(command, completed_command.returncode))
+            except subprocess.CalledProcessError as e:
+                error_message = "ERROR: {}".format(str(e))
+                raise Exception(error_message)
 
 
 if __name__ == '__main__':
