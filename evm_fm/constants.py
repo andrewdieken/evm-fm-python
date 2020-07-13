1f587c7b9b61307a8531043286f3eab9be2d8926
andrewdieken <andrewrd@live.com>
2020-07-12 20:02:45 -0700

    Add default configurations constants

---
+++ evm_fm/constants.py
@@ -0,0 +1,5 @@
+# Default Configurations
+DEFAULT_PYTHON_PATH = '/usr/bin/python'
+DEFAULT_STD_OUT_LOG_LOC = '/usr/local/bin/log/env_var_manager.log'
+DEFAULT_STSD_ERR_LOG_LOC = '/usr/local/bin/log/env_var_manager.log'
+DEFAULT_START_INTERVAL = 120
